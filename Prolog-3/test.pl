cb29201260ee377e5c61a887121c644c6d4f23d7
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2017-01-19 14:55:12 +0100

    Update

--- Prolog-3/test.pl
+++ Prolog-3/test.pl
@@ -26,3 +26,43 @@ podzbior([],Y).
 podzbior([A|X],Y):-
 	nalezy_do_listy(A,Y),
 	podzbior(X,Y).
+
+
+czesc_wspolna([],X,[]).
+czesc_wspolna([X|R],Y,[X|Z]):-
+	nalezy_do_listy(X,Y),
+	!,
+	czesc_wspolna(R,Y,Z).
+czesc_wspolna([X|R],Y,Z):-
+	czesc_wspolna(R,Y,Z).
+
+
+suma_elementow_list([X],[Y],[Z]):-
+	Z is X+Y.
+suma_elementow_list([HeadX|TailX],[HeadY|TailY],[HeadZ|TailZ]):-
+	HeadZ is HeadX + HeadY,
+	suma_elementow_list(TailX, TailY, TailZ).
+
+
+% add_matrices([Head_X,Sub_X|[Tail_X]],[Head_Y,Sub_Y|[Tail_Y]],Result):-
+% 	suma_elementow_list(Head_X, Head_Y, Temp),
+% 	suma_elementow_list(Sub_X, Sub_Y, Temp2),
+% 	append(Temp, Temp2, Temp_result),
+% 	write(Temp_result),
+% 	add_matrices(Tail_X, Tail_Y, Temp_result).
+
+% add_matrices([[X]],[[Y]],[[Z]]):-
+% 	Z is X+Y.
+%
+% add_matrices([[]],[[]],[[]]).
+%
+% add_matrices(Mat_X, Mat_Y, Mat_Z):-
+% 	[Head_X|Tail_X] = Mat_X,
+% 	[Head_Y|Tail_Y] = Mat_Y,
+% 	suma_elementow_list(Head_X, Head_Y, Head_Z),
+% 	append([Head_Z], Mat_Z, Result),
+% 	write(Result),
+% 	add_matrices(Tail_X, Tail_Y, Result).
+
+
+% add_matrices([ [2,3],[2,3] ], [ [3,4],[3,5] ], Z).
